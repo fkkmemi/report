01713ad9157ad80834deb44b92d6d2ab3ec52621
fkkmemi <fkkmemi@gmail.com>
2019-10-08 01:13:00 +0900

    65 응용편 주간보고 입력 만들기

--- src/views/reports/week.vue
+++ src/views/reports/week.vue
@@ -39,82 +39,108 @@
               <v-date-picker v-model="date" @input="menu = false"></v-date-picker>
             </v-menu>
           </v-col>
-          <v-col cols="12" sm="6" v-for="i in 2" :key="i">
+          <v-col cols="12" sm="6">
             <v-card>
               <v-subheader>지난주</v-subheader>
               <v-divider></v-divider>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    가나다라마바사
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    ijwiejgiojwoejgiawje;ofijawi;erja;lweira;lweijra;lwiejr;awefweflwiejrwegweg
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-icon>
-                  <v-progress-circular
-                    :rotate="270"
-                    :size="50"
-                    :width="10"
-                    :value="25"
-                    color="pink"
-                  >
-                    100
-                  </v-progress-circular>
-
-                </v-list-item-icon>
-              </v-list-item>
-              <v-divider></v-divider>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    가나다라마바사
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    ijwiejgiojwoejgiawje;ofijawi;erja;lweira;lweijra;lwiejr;awefweflwiejrwegweg
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-icon>
-                  <v-progress-circular
-                    :rotate="270"
-                    :size="50"
-                    :width="10"
-                    :value="40"
-                    color="pink"
-                  >
-                    40
-                  </v-progress-circular>
-
-                </v-list-item-icon>
-              </v-list-item>
-              <v-divider></v-divider>
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    가나다라마바사
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    ijwiejgiojwoejgiawje;ofijawi;erja;lweira;lweijra;lwiejr;awefweflwiejrwegweg
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-icon>
-                  <v-progress-circular
-                    :rotate="270"
-                    :size="50"
-                    :width="10"
-                    :value="25"
-                    color="pink"
-                  >
-                    25
-                  </v-progress-circular>
-
-                </v-list-item-icon>
-              </v-list-item>
-
+              <template three-line v-for="(item, i) in report.prevs">
+                <v-list-item :key="i">
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ item.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      {{ item.content }}
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                    <v-progress-circular
+                      :rotate="270"
+                      :size="50"
+                      :width="10"
+                      :value="item.progress"
+                      color="pink"
+                    >
+                      {{ item.progress }}
+                    </v-progress-circular>
+                  </v-list-item-icon>
+                </v-list-item>
+                <v-divider :key="i+1000000"></v-divider>
+              </template>
             </v-card>
           </v-col>
+          <v-col cols="12" sm="6">
+            <v-card>
+              <v-subheader>다음주</v-subheader>
+              <v-divider></v-divider>
+              <template three-line v-for="(item, i) in report.nexts">
+                <v-list-item :key="i">
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ item.title }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      {{ item.content }}
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                    <v-progress-circular
+                      :rotate="270"
+                      :size="50"
+                      :width="10"
+                      :value="item.progress"
+                      color="pink"
+                    >
+                      {{ item.progress }}
+                    </v-progress-circular>
+                  </v-list-item-icon>
+                </v-list-item>
+                <v-divider :key="i+1000000"></v-divider>
+              </template>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-card>
+              <v-subheader>지난주</v-subheader>
+              <template v-for="(item, i) in report.prevs">
+                <v-card-text :key="i">
+                  <v-text-field v-model="item.title" label="title"></v-text-field>
+                  <v-textarea v-model="item.content" label="content"></v-textarea>
+                  <v-slider v-model="item.progress" label="progress"></v-slider>
+                </v-card-text>
+                <v-divider :key="i+1000000"></v-divider>
+              </template>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon @click="add('prev')">
+                  <v-icon>mdi-file-plus-outline</v-icon>
+                </v-btn>
 
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-card>
+              <v-subheader>다음주</v-subheader>
+              <template v-for="(item, i) in report.nexts">
+                <v-card-text :key="i">
+                  <v-text-field v-model="item.title" label="title"></v-text-field>
+                  <v-textarea v-model="item.content" label="content"></v-textarea>
+                  <v-slider v-model="item.progress" label="progress"></v-slider>
+                </v-card-text>
+                <v-divider :key="i+1000000"></v-divider>
+              </template>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon @click="add('next')">
+                  <v-icon>mdi-file-plus-outline</v-icon>
+                </v-btn>
+
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-row>
 
       </v-card-text>
@@ -128,7 +154,39 @@ export default {
       menu: false,
       date: this.$moment().format('YYYY-MM-DD'),
       category: '생산',
-      categoryItems: ['연구소', '생산', 'QC']
+      categoryItems: ['연구소', '생산', 'QC'],
+      report: {
+        category: this.category,
+        date: this.date,
+        prevs: [
+          {
+            title: '',
+            content: '',
+            progress: 0
+          }
+        ],
+        nexts: [
+          {
+            title: '',
+            content: '',
+            progress: 0
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    add (type) {
+      let job
+      if (type === 'prev') job = this.report.prevs
+      else job = this.report.nexts
+      job.push(
+        {
+          title: '',
+          content: '',
+          progress: 0
+        }
+      )
     }
   }
 }
